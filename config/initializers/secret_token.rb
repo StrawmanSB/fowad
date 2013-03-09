2c2fe4bf5cd1d81bfe8228abd7d1a658cacc9c85
Luis Romero <luigi.romer@gmail.com>
2013-03-08 21:05:03 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fowad::Application.config.secret_token = '288eceef0820c50ac0f8572b466c4c7e4ae3f8ce739d5afbaabff36c3798cffd153dda7290758437539d26da7bb189706d687cd3118b636c2587bd09aa62b411'
